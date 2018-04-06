eb3f7f79f951304ca0cf37cb8eb66dd7abc9becd
Sergey Leskov <sergeylsls@gmail.com>
2018-04-06 15:05:41 +0300

    Initial Commit

---
+++ Curogram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Curogram
+//
+//  Created by Sergey Leskov on 4/6/18.
+//  Copyright © 2018 Sergey Leskov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
